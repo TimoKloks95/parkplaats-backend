38ff6e073d5a7888bd4beb4bf49434a77bbfdd31
TimoKloks95 <timokloks@hotmail.com>
2025-04-14 18:14:03 +0200

    First commit.

---
+++ src/main/java/com/timokloks/parkplaats/ParkplaatsApplication.java
@@ -0,0 +1,13 @@
+package com.timokloks.parkplaats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkplaatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkplaatsApplication.class, args);
+	}
+
+}
